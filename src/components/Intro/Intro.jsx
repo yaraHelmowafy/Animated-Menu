544a945be630de85051bf1aca3d0096a348af535
yaraHelmowafy <https://github.com/yaraHelmowafy>
2023-10-30 23:51:00 +0200

    first commit

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,9 @@
+import './intro.css'
+
+export default function Intro() {
+  return (
+    <div className='intro' id="intro">
+      <p className='name'> Intro Page </p>
+    </div>
+  )
+}
---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,9 @@
+import './testimonials.css'
+
+export default function Testimonials() {
+  return (
+    <div className='testimonials' id="testimonials">
+     <p className='name'> Testimonials Page </p>
+    </div>
+  )
+}
